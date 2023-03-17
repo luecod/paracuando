c2da62d7edc192b016ed4c82067def678e9997ff
Luis Valladolid <82429068+luecore@users.noreply.github.com>
2023-03-16 23:59:19 -0500

    style: formatting

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,3 +1,12 @@
 export function Footer() {
   return <div>FOOTER</div>;
 }
+// const Footer = () => {
+//   return (
+//     <div className="bg-black text-white flex items-center justify-between px-4 sm:px-12 py-4 min-h-[70px] text-sm">
+//       FOOTER
+//     </div>
+//   );
+// };
+
+// export default Footer;
--- components/navigation/header/Header.tsx
+++ components/navigation/header/Header.tsx
@@ -1,6 +1,8 @@
 const Header = () => {
   return (
-    <div className="bg-black text-white flex items-center justify-between px-4 sm:px-12 py-4 min-h-[70px] text-sm"></div>
+    <div className="bg-black text-white flex items-center justify-between px-4 sm:px-12 py-4 min-h-[70px] text-sm">
+      HEADER
+    </div>
   );
 };
 
