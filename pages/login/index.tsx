2e2679557aa11c332c2a35707987ee83fa31e066
Oscar Daniel Acho <oscaracho132@gmail.com>
2023-03-20 16:36:23 -0400

    feat: createRoutes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,7 +4,7 @@ import { NextPageWithLayout } from '../page';
 const LoginPage: NextPageWithLayout = () => {
   return (
     <div>
-      <h1>LOGIN</h1>
+      <h1>LOGIN:</h1>
       <Link href={'/login/create-account'}>create account</Link>
     </div>
   );
