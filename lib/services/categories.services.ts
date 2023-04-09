19fdb74cf9659339ffe011748a556e8737cd5714
Luis Valladolid <82429068+luecore@users.noreply.github.com>
2023-04-09 00:55:06 -0500

    feat: finished axios config

--- lib/services/categories.services.ts
+++ lib/services/categories.services.ts
@@ -1,15 +1,13 @@
-import useSWR from 'swr';
-import { CategoriesResponse } from '../interfaces/categories.interface';
+
 function useCategories() {
-  const { data, error, isLoading, mutate } = useSWR<CategoriesResponse>(
-    '/publications-types'
-  );
-  return {
-    data: data,
-    error,
-    isLoading,
-    mutate,
-  };
+  // const { data, error, isLoading, mutate } = useSWR<CategoriesResponse>(
+  //   '/publications-types');
+  // return {
+  //   data: data,
+  //   error,
+  //   isLoading,
+  //   mutate,
+  // };
 }
 
 export { useCategories };
--- lib/services/publications.services.ts
+++ lib/services/publications.services.ts
@@ -1,9 +1,12 @@
 import useSWR from "swr";
+import { PublicationsResponse } from "../interfaces/publications.interface";
+// import axios from "../helpers/axios.helper";
 
-function usePublications() {
+
+function usePublications(params?: string) {
   // const { data, error, isLoading, mutate } = useSWR('/publications', axios.get);
-  const { data, error, isLoading, mutate } = useSWR('/publications');
-  return { data, error, isLoading, mutate };
+  const { data, error, isLoading } = useSWR<PublicationsResponse>('/publications');
+  return { data: data?.results, error, isLoading };
   // return {
   //   publications: data,
   //   isLoading: !error && !data,
